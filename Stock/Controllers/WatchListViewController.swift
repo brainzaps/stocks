05131fd368746288ce9d7c2b8dedbf98fe37264f
brainzaps <skrypak.cookiedev@gmail.com>
2024-03-06 00:18:09 +0200

    feat: init

---
+++ Stock/Controllers/WatchListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stock
+//
+//  Created by Konstantin Skrypak on 05.03.2024.
+//
+
+import UIKit
+
+class WatchListViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		view.backgroundColor  = .red
+	}
+
+
+}
+
