24c7b1f3a260309d9f073613f5226efaf5ba4c3f
brainzaps <skrypak.cookiedev@gmail.com>
2024-03-06 00:48:36 +0200

    feat: create persistent manager

--- Stock/Managers/PersistenceManager.swift
+++ Stock/Managers/PersistenceManager.swift
@@ -7,3 +7,29 @@
 
 import Foundation
 
+final class PersistenceManager {
+	static let shared = PersistenceManager()
+	
+	private let userDefaults: UserDefaults = .standard
+	
+	private struct Constants {}
+	
+	init () {}
+	
+	// MARK: - Public
+	
+	public var watchlist: [String] {
+		return []
+	}
+	
+	public func addToWatchlist() {}
+	
+	public func removeFromWatchlist() {}
+	
+	// MARK: - Private
+	
+	
+	private var hasOnboarded: Bool {
+		return false
+	}
+}
