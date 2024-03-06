484e6367a602a231720a7c646cb92bd576f00d05
brainzaps <skrypak.cookiedev@gmail.com>
2024-03-06 23:16:44 +0200

    feat: create list of stocks

--- Stock/Controllers/WatchListViewController.swift
+++ Stock/Controllers/WatchListViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import Combine
 
 class WatchListViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		view.backgroundColor  = .red
+		view.backgroundColor  = .systemBackground
+		
+		setUpSearchController()
+		setUpTitleView()
+	}
+	
+	private func setUpSearchController() {
+		let resultVC = SearchResultsViewController()
+		let searchVC = UISearchController(searchResultsController: resultVC)
+		searchVC.searchResultsUpdater = self
+		
+		navigationItem.searchController = searchVC
+	}
+	
+	private func setUpTitleView() {
+		let titleView = UIView(frame: CGRect(x: 0, y: 0, width: view.width, height: navigationController?.navigationBar.height ?? 100))
+		
+		let label = UILabel(frame: CGRect(x: 10, y: 0, width: titleView.width - 20, height: titleView.height))
+		label.text = "Stocks"
+		label.font  = .systemFont(ofSize: 32, weight: .medium)
+		
+		titleView.addSubview(label)
+		navigationItem.titleView = titleView
 	}
 
+}
+
+extension WatchListViewController: UISearchResultsUpdating {
+	func updateSearchResults(for searchController: UISearchController) {
+		guard let query = searchController.searchBar.text,
+					let resultVC = searchController.searchResultsController as? SearchResultsViewController,
+					!query.trimmingCharacters(in: .whitespaces).isEmpty else { return }
+		
+	}
+	
+	
 }
 
